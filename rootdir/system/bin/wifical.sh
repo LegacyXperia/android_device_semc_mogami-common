5af1fe3cef8a661b639a4aee79c6d05e85bef283
Michael Bestas <mikeioannina@gmail.com>
2013-10-01 15:20:45 +0300

    calibrator: temp workaround to fix calibration
    for some reason it doesn't work
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- rootdir/system/bin/wifical.sh
+++ rootdir/system/bin/wifical.sh
@@ -3,7 +3,7 @@
 WIFION=`getprop init.svc.p2p_supplicant`
 TARGET_FW_DIR=/system/etc/firmware/ti-connectivity
 TARGET_NVS_FILE=$TARGET_FW_DIR/wl1271-nvs.bin
-TARGET_INI_FILE=/system/etc/tiwlan.ini
+TARGET_INI_FILE=/system/etc/wifi/TQS_S_2.6.ini
 WL12xx_MODULE=/system/lib/modules/wl12xx_sdio.ko
 HW_MAC=`cat /proc/cmdline | grep -o -E "wifi0.eth_addr=([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}" | sed s/wifi0.eth_addr=//`
 
@@ -32,12 +32,17 @@ fi
 mount -o remount rw /system
 
 # Remove old NVS file
-rm /system/etc/firmware/ti-connectivity/wl1271-nvs.bin
+if [ -e $TARGET_NVS_FILE ];
+then
+    rm $TARGET_NVS_FILE
+fi
 
 # Actual calibration...
 # calibrator plt autocalibrate <dev> <module path> <ini file1> <nvs file> <mac addr>
 # Leaving mac address field empty for random mac
-calibrator plt autocalibrate wlan0 $WL12xx_MODULE $TARGET_INI_FILE $TARGET_NVS_FILE $HW_MAC
+nvimport
+calibrator set upd_nvs $TARGET_INI_FILE /data/etc/wifi/fw $TARGET_NVS_FILE
+calibrator set nvs_mac $TARGET_NVS_FILE $HW_MAC
 
 echo " ******************************"
 echo " * Finished Wi-Fi calibration *"
