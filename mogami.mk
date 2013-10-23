96331edba38942392ca80ff8174f761821ff27e3
Michael Bestas <mikeioannina@gmail.com>
2013-10-23 23:17:08 +0300

    mogami: Enable Bluetooth LE
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>

--- mogami.mk
+++ mogami.mk
@@ -21,6 +21,7 @@ DEVICE_PACKAGE_OVERLAYS += device/semc/mogami-common/overlay
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml
 
