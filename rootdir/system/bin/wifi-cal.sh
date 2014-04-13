bd7c7d9b8606e2e5d0d3a11b12171f6e0d200aef
Michael Bestas <mikeioannina@gmail.com>
2014-04-13 23:19:10 +0300

    wl12xx: Leave wifi module always loaded.
    * Time needed to enable wifi is much shorter with this

--- rootdir/system/bin/wifi-cal.sh
+++ rootdir/system/bin/wifi-cal.sh
@@ -21,8 +21,7 @@ case "$WIFION" in
   "running") echo " ****************************************"
              echo " * Turning Wi-Fi off before calibration *"
              echo " ****************************************"
-             svc wifi disable
-             rmmod $WL12xx_MODULE;;
+             svc wifi disable;;
           *) echo " ******************************"
              echo " * Starting Wi-Fi calibration *"
              echo " ******************************";;
@@ -30,7 +29,7 @@ esac
 
 if [ -e $WL12xx_MODULE ];
 then
-    echo ""
+    rmmod $WL12xx_MODULE
 else
     echo "*********************************"
     echo "* wl12xx_sdio module not found! *"
@@ -45,6 +44,8 @@ nvimport > /dev/null 2>&1
 calibrator set upd_nvs $TARGET_INI_FILE /data/etc/wifi/fw $TARGET_NVS_FILE
 calibrator set nvs_mac $TARGET_NVS_FILE $HW_MAC
 setprop persist.wlan.ti.calibrated 1
+
+insmod $WL12xx_MODULE
 
 echo " ******************************"
 echo " * Finished Wi-Fi calibration *"
